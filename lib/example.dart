966164c196b0092674d401236e8f85c6a19a1d0b
JoseCelis0005 <jmcelis0005@gmail.com>
2025-04-16 19:39:34 -0500

    JCELIS

--- lib/example.dart
+++ lib/example.dart
@@ -18,7 +18,6 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-
 class _MyHomePageState extends State<MyHomePage> {
   int _selectedIndex = 0;
 
