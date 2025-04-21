0e588b640c9a11c4466d4b2109152f69d7fb1928
JoseCelis0005 <jmcelis0005@gmail.com>
2025-04-20 19:33:19 -0500

    Celis Editar Opciones Menu

---
+++ lib/widgets/menu_inferior.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class CustomBottomNav extends StatelessWidget {
+  final int currentIndex;
+  final Function(int) onTap;
+
+  const CustomBottomNav({
+    Key? key,
+    required this.currentIndex,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: onTap,
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.arrow_back),
+          label: 'Atrás',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Inicio',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.logout),
+          label: 'Salir',
+        ),
+      ],
+    );
+  }
+}
